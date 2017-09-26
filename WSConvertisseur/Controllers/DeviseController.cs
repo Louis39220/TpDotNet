fa55970e6a889b04fed18d799ea46fefd8882808
Louis39220 <you@example.com>
2017-09-26 15:17:18 +0200

    iiii

---
+++ WSConvertisseur/Controllers/DeviseController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WSConvertisseur.Models;
+
+namespace WSConvertisseur.Controllers
+{
+    public class DeviseController : ApiController
+    {
+
+        List<Devise> listDevises;
+
+        public DeviseController()
+        {
+            listDevises = new List<Devise>();
+
+            listDevises.Add(new Devise(1, "Dollar", 1.08));
+            listDevises.Add(new Devise(2, "Franc Suisse", 1.07));
+            listDevises.Add(new Devise(3, "Yen", 120));
+        }
+
+        // GET: api/Devise
+        public IQueryable<Devise> Get()
+        {
+            return listDevises.AsQueryable();
+        }
+
+        // GET: api/Devise/5
+        [ResponseType(typeof(Devise))]
+        public IHttpActionResult Get(int id)
+        {
+            Devise devise = listDevises.FirstOrDefault((d) => d.id == id);
+            if(devise == null)
+            {
+                return NotFound();
+            }
+            return Ok(devise);
+        }
+
+        // POST: api/Devise
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Devise/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Devise/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        public int id { get; set; }
+        public string nom { get; set; }
+        public double taux { get; set; }
+
+        public Devise()
+        {
+
+        }
+
+        public Devise(int id, string nom, double taux)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.taux = taux;
+        }
+
+        
+
+    }
+}
